dc15b6ab37b048914611d52a6b048b5d355b825a
Leonie Hannig <mail@leoniehannig.de>
2019-03-18 14:24:01 +0100

    alembic setup

---
+++ alembic/versions/9fa3fb1e3212_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 9fa3fb1e3212
+Revises: 
+Create Date: 2019-03-18 14:22:46.999713
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9fa3fb1e3212'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- cute_wooly_mammoth.py
+++ cute_wooly_mammoth.py
@@ -1,7 +1,7 @@
 import os
-from sqlalchemy import create_engine
+import sqlalchemy
+from alembic import op
 from models import *
-
 
 
 def setup_db():
